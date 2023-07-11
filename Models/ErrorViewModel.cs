73a4801acb7f0e6e9027e6a121c79a691202a75b
Karthikeyan Arumugam <karthikeyan.arumugam@phlashweb.co.uk>
2023-07-11 14:01:29 +0100

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RevisionNotes.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
